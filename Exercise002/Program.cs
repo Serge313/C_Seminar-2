ee766fd02fefd54cf1ed156a7c9f25f26ae5fba8
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-11 04:42:11 +0300

    Exercise002 complited

---
+++ Exercise002/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задание: написать программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+int GetRandomNumber(int min, int max)
+{
+    return new Random().Next(min, max + 1);
+}
+
+int randomNumber = GetRandomNumber(100, 999);
+Console.WriteLine(randomNumber);
+
+int firstDigit = randomNumber / 100;
+int thirdDigit = randomNumber % 10;
+
+string GetRest(int a, int b)
+{
+    string remain1 = a.ToString();
+    string remain2 = remain1 + b.ToString();
+    return remain2;
+}
+
+string rest = GetRest(firstDigit, thirdDigit);
+Console.WriteLine(rest);
