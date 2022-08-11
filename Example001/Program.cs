cf964b4e0f25bbf94acc74a1608e8879af2622cc
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-11 03:27:12 +0300

    Example001 completed

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int TestFunction(int a, int b)
+{
+    return a + b;
+}
+
+Console.WriteLine(TestFunction(4, 5));
+
+//процедуры - функции, которые ничего не возвращают, пример: void
+
+//void TestProcedure(int a, int b);
+//{
+//
+//}
