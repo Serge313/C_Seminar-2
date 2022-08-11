37019059d39294270c2adf3b026bf4513d66ce34
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-11 04:11:57 +0300

    Exercise001 complited

---
+++ Exercise001/Program.cs
@@ -0,0 +1,11 @@
+﻿int GetRandomNumber(int min, int max)
+{
+    return new Random().Next(min, max + 1);
+}
+
+int randomNumber = GetRandomNumber(100, 999);
+Console.WriteLine(randomNumber);
+
+int rest = randomNumber % 100;
+
+Console.WriteLine(rest);
