74fd7d1a742640fd8dbeef734a710e062d4402e2
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-11 05:19:10 +0300

    Exercise003 complited

---
+++ Exercise003/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задание: написать программу, которая будет принимать на вход два числа и выводить является ли первое число кратным второму.
+
+int GetRandomNumber(int min, int max)
+{
+    return new Random().Next(min, max + 1);
+}
+
+int firstRandomNumber = GetRandomNumber(1, 999);
+int secondRandomNumber = GetRandomNumber(1, 999);
+
+Console.WriteLine(firstRandomNumber);
+Console.WriteLine(secondRandomNumber);
+
+int GetAliquot(int a, int b)
+{
+    int remain = 0;
+    if(a % b == 0)
+    {
+        remain = 0;
+        Console.WriteLine("Кратно");
+    }
+    else if(a < b)
+    {
+        Console.WriteLine("Error!!!");
+    }
+    else
+    {
+        remain = a % b;
+        Console.WriteLine("Не кратно, остаток " + remain);
+    }
+
+    return remain;
+}
+
+int result = GetAliquot(firstRandomNumber, secondRandomNumber);
