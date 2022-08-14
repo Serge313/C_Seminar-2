21f55b92c8efe5440644b0a7f866b4a794698764
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-14 13:23:46 +0300

    Exercise004 completed

---
+++ Exercise004/Program.cs
@@ -0,0 +1,18 @@
+﻿int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int number = GetNumber("Введите число ");
+
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Yep");
+} 
+else
+{
+    Console.WriteLine("Nope");
+}
