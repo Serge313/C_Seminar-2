948fc013e9ece4610fcb062ec236e2c2e80562b4
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-14 13:55:45 +0300

    Exercise005 completed

---
+++ Exercise005/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int number1 = GetNumber("Enter your first number ");
+int number2 = GetNumber("Enter your second number ");
+
+
+if (number1 * number1 == number2 || number2 * number2 == number1)
+{
+    Console.WriteLine("Yep");
+}
+else
+{
+    Console.WriteLine("Nope");
+} 
