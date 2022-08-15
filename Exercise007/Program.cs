3726dc2365de98ca1f41846de47d1f9ebeb90610
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-15 19:38:36 +0300

    Exercise007 completed

---
+++ Exercise007/Program.cs
@@ -0,0 +1,29 @@
+﻿double firstDotFirstCoordinate = GetNumber("Enter the first coordinate of the first dot ");
+double firstDotSecondCoordinate = GetNumber("Enter the second coordinate of the first dot ");
+double secondDotFirstCoordinate = GetNumber("Enter the first coordinate of the second dot ");
+double secondDotSecondCoordinate = GetNumber("Enter the second coordinate of the second dot ");
+double firstCathetus = Cathetus(firstDotFirstCoordinate, secondDotFirstCoordinate);
+double secondCathetus = Cathetus(firstDotSecondCoordinate, secondDotSecondCoordinate);
+double distance = Distance(firstCathetus, secondCathetus);
+Console.WriteLine(distance);
+
+
+double Cathetus(double a, double b)
+{
+    double cathetus = (b - a);
+    return cathetus;
+}
+
+
+double Distance(double a, double b)
+{
+    double c = Math.Sqrt((a * a) + (b * b));
+    return c;
+}
+
+
+double GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
