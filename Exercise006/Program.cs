dd036c83ed07a588b2652e4c2009775c6ae716a6
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-15 19:01:50 +0300

    Exercise006 completed

---
+++ Exercise006/Program.cs
@@ -0,0 +1,31 @@
+﻿int x = GetNumber("Enter your X ");
+int y = GetNumber("Enter your Y ");
+
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("Enter X and Y, wich are not equal to zero");
+    return;
+}
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("The dot is in the first quarter");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("The dot is in the second quarter");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("The dot is in the third quarter");
+}
+else
+{
+    Console.WriteLine("The dot is in the fourth quarter");
+}
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
