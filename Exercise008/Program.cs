1f0c67026ac3249cbc5f9317f0826bda48896a95
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-17 21:00:33 +0300

    Exercise008 and Exercise009 completed

---
+++ Exercise008/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36*/
+
+/*
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+int sum = 0;
+
+while (index <= number)
+{
+    sum += index++;
+}
+Console.WriteLine(sum);*/
+
---
+++ Exercise009/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (number > 0)
+{
+    i++;
+    number = number / 10;
+}
+Console.WriteLine(i);
