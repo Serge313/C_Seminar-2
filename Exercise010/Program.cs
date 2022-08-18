856a34c99bbebdbb49ac22c82a15ab2319d99661
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-18 20:54:46 +0300

    Exercise010 completed

---
+++ Exercise010/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.*/
+
+int size = ReadInt("Введите размер массива: ");
+int[] array = new int[size];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+MirrorArray(array);
+PrintArray(array);
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+        array1[i] = new Random().Next(-9, 10);
+}
+
+void MirrorArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+        array1[i]  *= (-1);
+}
+
+void PrintArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+        Console.Write(array1[i]+ " ");
+    Console.WriteLine();
+}
