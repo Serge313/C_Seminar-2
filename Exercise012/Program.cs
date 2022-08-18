50cc94ba14446e659a5ba079507fb5ce478b0c46
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-18 22:04:27 +0300

    Exercise012 completed

---
+++ Exercise012/Program.cs
@@ -0,0 +1,33 @@
+﻿void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 201);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountNeeded(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] < 100)
+            counter++;
+    }
+    return counter;
+}
+
+int[] numbers = new int[123];
+FillArrayRandomNumbers(numbers);
+int result = CountNeeded(numbers);
+PrintArray(numbers);
+Console.Write($"\nВ заданном массиве {result} чисел от 10 до 99");
